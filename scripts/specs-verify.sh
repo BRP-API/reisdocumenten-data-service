3d7f8f435736e870e7105d024f96145886953707
MelvLee <github-action@actions-template-sync.noreply.github.com>
2024-05-17 00:03:44 +0000

    chore: sync met 'BRP-API/brp-shared' repo
    Signed-off-by: MelvLee <github-action@actions-template-sync.noreply.github.com>

--- scripts/specs-verify.sh
+++ scripts/specs-verify.sh
@@ -5,9 +5,3 @@ npx cucumber-js -f json:./test-reports/cucumber-js/step-definitions/test-result-
                 -f summary \
                 features/docs \
                 --tags "not @integratie"
-
-npx cucumber-js -f json:./test-reports/cucumber-js/reisdocumenten/test-result.json \
-                -f summary:./test-reports/cucumber-js/reisdocumenten/test-result-summary.txt \
-                -f summary \
-                features/raadpleeg-met-reisdocumentnummer \
-                features/zoek-met-burgerservicenummer
