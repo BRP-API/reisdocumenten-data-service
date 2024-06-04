699a0c3f53ea570fab8486f3dd206aa991c129c7
Melvin Lee <melvin.lee@iswish.nl>
2024-06-04 08:46:38 +0200

    ci: containers-stop script toegevoegd

---
+++ scripts/containers-stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MODE=$1
+
+if [ "$MODE" = "ci" ]; then
+    docker compose -f .docker/db-ci.yml down
+else
+    docker compose -f .docker/db.yml down
+fi
+
+docker compose -f .docker/reisdocumenten-data-service.yml down
