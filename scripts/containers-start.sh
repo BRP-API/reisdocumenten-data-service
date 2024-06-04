1110ad2253052836863ff01031f6afcfb052c125
Melvin Lee <melvin.lee@iswish.nl>
2024-06-04 08:40:37 +0200

    ci: refactored tbv gebruik reusable actions

---
+++ scripts/containers-start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MODE=$1
+
+if [ "$MODE" = "ci" ]; then
+    docker compose -f .docker/db-ci.yml up -d
+else
+    docker compose -f .docker/db.yml up -d
+fi
+
+docker compose -f .docker/reisdocumenten-data-service.yml up -d
--- scripts/start-containers.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker compose -f .docker/db.yml -f .docker/reisdocumenten-data-service.yml up -d
