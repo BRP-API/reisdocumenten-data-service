f7d561b634f39028168366b748b71c9233537849
Melvin Lee <melvin.lee@iswish.nl>
2024-08-05 18:14:08 +0200

    ci: update data-service manifest nav nieuwe versie

--- scripts/containers-stop.sh
+++ scripts/containers-stop.sh
@@ -3,9 +3,9 @@
 MODE=$1
 
 if [ "$MODE" = "ci" ]; then
-    docker compose -f .docker/db-ci.yml down
+    docker compose -f .docker/db-ci.yml down --volumes
 else
-    docker compose -f .docker/db.yml down
+    docker compose -f .docker/db.yml down --volumes
 fi
 
-docker compose -f .docker/reisdocumenten-data-service.yml down
+docker compose -f .docker/reisdocumenten-data-service.yml down --volumes
